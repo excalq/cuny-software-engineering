d4ef0cc48bd6e1ef1bd120c0e6f3c0f37936104d
Arthur Ketcham <arthur@plexusentertainment.com>
2013-03-08 19:32:20 -0500

    updated again for class

--- javascript.js
+++ javascript.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
 
-  $('input.add_text').click(function() {
-    var one = $('#one');
-    var newtext = "<br>This is some new text dynamically added!";
+  var one = $('#one');
+  var two = $('#two');
+  var newtext = "<br>This is some new text dynamically added!";
+  $('input#add_text').click(function() {
     one.append(newtext);
-    
+  });
+  $('input.add_text').click(function() {
+    two.append(newtext);
+    two.css('background', 'url("http://michaeldoret.com/doret/artwork/knicks.gif")');
   });
 
   
